acbbd80466bb58996ab1cd259807e6c67f652beb
Folkert-Jan van der Pol <folkertjanvanderpol@gmail.com>
2019-01-24 11:41:39 +0100

    Added scrolltoplugin

--- src/modules/animation/scroll.js
+++ src/modules/animation/scroll.js
@@ -1,10 +1,9 @@
-import TweenLite from 'gsap/TweenLite'
-import 'gsap/ScrollToPlugin'
+import TweenLite from 'gsap/all'
 
 const scroll = href => {
   const link = href.split('/')
   const section = document.querySelector(link[link.length - 1])
   const page = document.querySelector('#main')
-  TweenLite.to(page, 1.2, { scrollTo: { x: 0, y: section.offsetTop } })
+  TweenLite.to(page, 1.2, { scrollTo: section })
 }
 export default { scroll }
