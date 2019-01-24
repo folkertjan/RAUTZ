932063310ee3c8c3daa8a10b1f2083fcea142317
Folkert-Jan van der Pol <folkertjanvanderpol@gmail.com>
2019-01-24 11:35:54 +0100

    trying scroll again

--- src/modules/animation/scroll.js
+++ src/modules/animation/scroll.js
@@ -3,7 +3,6 @@ import 'gsap/ScrollToPlugin'
 
 const scroll = href => {
   const link = href.split('/')
-  console.log(link, link[link.length - 1])
   const section = document.querySelector(link[link.length - 1])
   const page = document.querySelector('#main')
   TweenLite.to(page, 1.2, { scrollTo: { x: 0, y: section.offsetTop } })
